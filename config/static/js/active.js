3e0a95d93be154f1f65dc1fa1dc7fd7a41e00d91
Alexander <alexander@iMac-Alexander.local>
2024-01-23 12:37:18 +0300

    active link

--- config/static/js/active.js
+++ config/static/js/active.js
@@ -1,26 +1,14 @@
 $(document).ready(function () {
-//    $('.my-slider').unslider({
-//        animation: 'vertical',
-//        autoplay: true,
-//        infinite: true
-//    });
-//
-//
-//    $('.our-client-carousal').owlCarousel({
-//        items: 1,
-//        loop: true,
-//        autoplay: true,
-//        autoplayTimeout: 5000,
-//        margin: 10,
-//        nav: true,
-//        navText: [
-//        "<i class=\"fa fa-angle-left\" aria-hidden=\"true\"></i>",
-//        "<i class=\"fa fa-angle-right\" aria-hidden=\"true\"></i>"],
-//    });
-     var $grid = $('.grid').isotope({
-//        itemSelector: '.portfolio-item',
-        layoutMode: 'fitRows'
-    });
+
+    $('.menu a').each(function(){
+        var location = window.location.protocol+window.location.host+window.location.pathname;
+        console.log($(this).attr('href'));
+        console.log(window.location.pathname);
+        if($(this).attr('href') == window.location.pathname){
+            $(this).parent().addClass('active');
+        }
+    })
+
     var filterFns = {
         numberGreaterThan50: function () {
             var number = $(this).find('.number').text();
